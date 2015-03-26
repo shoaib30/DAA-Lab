96537908ee3f7095a64f71735d3a067c1c2ab8de
Shoaib Ahmed <shoaib30@gmail.com>
2015-03-26 23:50:29 +0530

    corrected best case function

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,28 +8,12 @@ int list[max_length];
 double total_time,start,end,d1;
 void bubbleSort()
 {
-    int temp;
-    for(long i=0;i<length;i++)
-    {
-        for(long j=0;j<length-i-1;j++)
-        {
-            if (list[j]>list[j+1])
-            {
-                temp=list[j];
-                list[j]=list[j+1];
-                list[j+1]=temp;
-            }
-        }
-    }
-}
-void bubbleSortBestCase()
-{
-    int temp;int flag=0;
+    int temp,flag=1;
     for(long i=0;i<length&&flag!=0;i++)
     {
+        flag=0;
         for(long j=0;j<length-i-1; j++)
         {
-         flag=0;
             if (list[j]>list[j+1])
             {
                 temp=list[j];
@@ -57,7 +41,7 @@ int main()
         total_time=((double)(end- start))/CLOCKS_PER_SEC;
         cout<<"\nAVG Time: "<<total_time;
         start=clock();
-        bubbleSortBestCase();
+        bubbleSort();
         end=clock();
         total_time=((double)(end- start))/CLOCKS_PER_SEC;
         cout<<"\nBEST Time: "<<total_time<<"\n";
